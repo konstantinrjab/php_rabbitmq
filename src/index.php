420ef674696d7591ee8e9b2160ff00f554575179
konstantinrjab <konstantinrjab@gmail.com>
2019-07-21 16:39:06 +0300

    added required php extensions

--- src/index.php
+++ src/index.php
@@ -1,5 +1,19 @@
 <?php
 
-$t = 12;
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
 
-echo '123123';
+try {
+    $connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
+    $channel = $connection->channel();
+
+    $channel->queue_declare('hello', false, false, false, false);
+
+    $msg = new AMQPMessage('Hello World!');
+    $channel->basic_publish($msg, '', 'hello');
+
+    echo " [x] Sent 'Hello World!'\n";
+} catch (Throwable $throwable) {
+    $t = 1;
+}
